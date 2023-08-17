062c4b8b32d7af2ac832a8ffb14a6cf4ff9dab1f
hoang1101 <lhhoang1101@gmail.com>
2023-08-17 20:38:55 +0700

    asdadsa

--- controllers/evaluate.controller.js
+++ controllers/evaluate.controller.js
@@ -117,25 +117,37 @@ exports.EditDanhGiaSanPham = async (req, res) => {
 exports.getEvaluateProduct = async (req, res) => {
   try {
     const { id_product } = req.params;
-    const evaluate = await db.Evaluate.findAll({
+    const evaluate = await db.Order_Item.findAll({
+      where: {
+        product_id: id_product,
+      },
+      attributes: [],
       include: [
+        {
+          model: db.Evaluate,
+          as: "evaluate",
+          attributes: [
+            "id",
+            "id_orderitem",
+            "customer_id",
+            "start",
+            "img",
+            "comment",
+            "date",
+          ],
+
+          include: [
+            {
+              model: db.Customer,
+              as: "customer",
+              attributes: ["fullname", "email", "phone"],
+            },
+          ],
+        },
         {
           model: db.Product,
           as: "product",
           attributes: ["name"],
-        },
-        {
-          model: db.Customer,
-          as: "customer",
-          attributes: ["fullname", "email", "phone"],
-        },
-        {
-          model: db.Order_Item,
-          as: "order_item",
-          attributes: [],
-          where: {
-            product_id: id_product,
-          },
         },
       ],
       raw: true,
