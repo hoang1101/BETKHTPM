a753c02850355ab1f329e50baf65afdf2b813831
huan572001 <85747713+huan572001@users.noreply.github.com>
2023-10-24 23:42:56 +0700

    setup deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
 
