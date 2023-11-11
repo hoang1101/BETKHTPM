b0bdbda921eb9a08af41eb01720745a1dd98d0e8
hoang1101 <lhhoang1101@gmail.com>
2023-11-11 13:12:25 +0700

    danhgia

--- controllers/evaluate.controller.js
+++ controllers/evaluate.controller.js
@@ -7,6 +7,7 @@ const config = require("../config/config");
 const {
   createEvaluateNoImgDao,
   updateEvaluateNoImgDao,
+  createEvaluateDao,
 } = require("../dao/evaluate.dao");
 exports.DanhGiaSanPham = async (req, res) => {
   try {
